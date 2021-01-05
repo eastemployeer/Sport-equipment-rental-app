cdc094978945a64ff44db4e26b84b349e6e27cad
Adam Jędryka <adam.jedryka98@gmail.com>
2021-01-05 21:09:54 +0100

    Lint

--- src/views/clients/MyCart.vue
+++ src/views/clients/MyCart.vue
@@ -87,9 +87,9 @@
 </template>
 
 <script lang='ts'>
-import { Component, Vue, Watch } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
-import { Product } from '@/models/Product';
+import Product from '@/models/Product';
 import EventBus from '@/services/EventBus';
 import store from '@/store';
 
